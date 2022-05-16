abdcffcf6232aa25677c5113cccc1183b6fc2677
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2022-05-16 19:53:37 +0000

    Add files via upload

---
+++ baz_setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -e
+
+main() {
+    echo 'Setting up baz'
+
+    echo 'Cloning repository'
+    git clone https://ari-web.xyz/gh/baz
+    cd baz
+
+    echo 'Installing baz'
+    su -c 'install -Dm755 baz /usr/bin'
+
+    echo 'Setting baz up'
+    baz setup
+
+    echo 'Adding lines to bashrc'
+    tee -a ~/.bashrc <<EOF
+export BAZ_LOADER_ENABLED=true
+[ -f "$HOME/.local/share/baz/loader.sh" ] && source "$HOME/.local/share/baz/loader.sh"
+EOF
+
+    echo 'Adding completions'
+    bash ./scripts/comp.sh
+
+    echo 'Done'
+}
+
+main "$@"
+
