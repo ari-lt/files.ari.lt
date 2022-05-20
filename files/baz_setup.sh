5bb282163c4b3b1d4bd72f41d9cdbdad92637620
TruncatedDinosour <71613062+TruncatedDinosour@users.noreply.github.com>
2022-05-20 17:55:02 +0000

    Update baz_setup.sh

--- files/baz_setup.sh
+++ files/baz_setup.sh
@@ -1,36 +1,46 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
 main() {
     echo 'Setting up baz'
+    export PREFIX="${PREFIX:-/usr/bin}"
 
     echo 'Pre-cleanup'
-    rm -rf baz
+    rm -rf -- baz
 
-    echo 'Cloning repository'
+    echo 'Cloning git repository'
     git clone https://ari-web.xyz/gh/baz
     cd baz
 
+    if [ ! -x 'baz' ]; then
+        echo 'Making baz executable'
+        chmod a+rx baz
+    fi
+
     echo 'Installing baz'
-    ${__BASH_RUNAS:-sudo} install -Dm755 baz /usr/bin
+    ${__BASH_RUNAS:-sudo} install -Dm755 baz "$PREFIX"
 
     echo 'Setting baz up'
-    baz setup
+    ./baz setup
 
-    echo 'Adding lines to bashrc'
-    tee -a ~/.bashrc <<EOF
+    read -rp 'Do you want to add lines to bashrc? [y/n] >>> ' yn
+
+    if [ "$yn" = 'y' ]; then
+        echo 'Adding lines to bashrc'
+        tee -a ~/.bashrc <<EOF
 # Load baz
 export BAZ_LOADER_ENABLED=true
 _baz_loader="$HOME/.local/share/baz/loader.sh"
 
 [ ! -f "\$_baz_loader" ] || source "\$_baz_loader"
 EOF
+    fi
 
     echo 'Adding completions'
     bash ./scripts/comp.sh
 
-    echo 'Done'
+    echo "Done setting up baz, make sure '$PREFIX' is in \$PATH"
 }
 
 main "$@"
